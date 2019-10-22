20941703f996f454f62fe5c0426db554b3731016
oumar <balloumar49@hotmail.com>
2019-10-22 10:17:00 +0200

    wip

--- Scan3D/mainwindow.cpp
+++ Scan3D/mainwindow.cpp
@@ -1,10 +1,5 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QApplication>
-#include <QFileDialog>
-#include <QFile>
-#include <QMessageBox>
-#include <QTextStream>
 
 #include <iostream>
 
@@ -28,10 +23,6 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-
-    QString fichier = QFileDialog::getOpenFileName(this, "Ouvrir un fichier", QString(), "Images (*.png *.gif *.jpg *.jpeg)");
-    QMessageBox::information(this, "Fichier", "Vous avez sélectionné :\n" + fichier);
-
     ui->setupUi(this);
     QObject::connect(ui->openFile, SIGNAL(triggered()), this, SLOT(ouvrirDialogue()));
 }
